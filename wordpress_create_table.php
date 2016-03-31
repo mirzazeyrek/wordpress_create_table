46e41249fa75c67479a8f22fea40cab070ed1896
uğur mirza zeyrek <github@mersinuni.net>
2016-03-31 15:40:52 +0300

    Update wordpress_create_table.php
    fixed indents.

--- wordpress_create_table.php
+++ wordpress_create_table.php
@@ -6,14 +6,14 @@
 	 * $table_name		= "ratings";
 	 *
 	 * $table_columns 	= "id INT(6) UNSIGNED AUTO_INCREMENT,
-	 * 					rate tinyint(1) NOT NULL,
-	 * 					ticket_id bigint(20) NOT NULL,
-	 * 					response_id bigint(20) NOT NULL,
-	 * 					created_at TIMESTAMP";
+	 * 			rate tinyint(1) NOT NULL,
+	 * 			ticket_id bigint(20) NOT NULL,
+	 * 			response_id bigint(20) NOT NULL,
+	 * 			created_at TIMESTAMP";
 	 *
 	 * $table_keys 		= "PRIMARY KEY (id),
-	 * 					KEY ratings_rate (rate),
-	 * 					UNIQUE KEY ratings_response_id (response_id)";
+	 * 			KEY ratings_rate (rate),
+	 * 			UNIQUE KEY ratings_response_id (response_id)";
 	 *
 	 * create_table($table_name, $table_columns, $table_keys);
 	 *
