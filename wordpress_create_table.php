7f2a2e0a0a800f900171a92ea5c6ffc9094b27eb
uğur mirza zeyrek <github@mersinuni.net>
2016-03-31 15:40:11 +0300

    Update wordpress_create_table.php
    added more information to docblock.
    
    added strip function for backticks.

--- wordpress_create_table.php
+++ wordpress_create_table.php
@@ -1,4 +1,3 @@
-<?php
 	/**
 	 * Prevents unnecessary re-creating index and repetitive altering table operations when using WordPress dbDelta function
 	 *
@@ -18,25 +17,48 @@
 	 *
 	 * create_table($table_name, $table_columns, $table_keys);
 	 *
+	 * Things that need to be considered when using dbDelta function :
+	 *
+	 * You must put each field on its own line in your SQL statement.
+	 * You must have two spaces between the words PRIMARY KEY and the definition of your primary key.
+	 * You must use the key word KEY rather than its synonym INDEX and you must include at least one KEY.
+	 * You must not use any apostrophes or backticks around field names.
+	 * Field types must be all lowercase.
+	 * SQL keywords, like CREATE TABLE and UPDATE, must be uppercase.
+	 * You must specify the length of all fields that accept a length parameter. int(11), for example.
+	 *
+	 * Further information can be found on here:
+	 *
+	 * http://codex.wordpress.org/Creating_Tables_with_Plugins
+	 *
 	 * @param $table_name
 	 * @param $table_columns
 	 * @param null $table_keys
 	 * @param null $charset_collate
-	 * @version 1.0 
+	 * @version 1.0.1
 	 * @author Ugur Mirza Zeyrek
-     */
-	function create_table($table_name, $table_columns, $table_keys = null, $db_prefix = true, $charset_collate = null) {
+	 */
+	static function create_table($table_name, $table_columns, $table_keys = null, $db_prefix = true, $charset_collate = null) {
 		global $wpdb;
 
 		if($charset_collate == null)
-		$charset_collate = $wpdb->get_charset_collate();
+			$charset_collate = $wpdb->get_charset_collate();
 		$table_name = ($db_prefix) ? $wpdb->prefix.$table_name : $table_name;
 		$table_columns = strtolower($table_columns);
 
 		if($table_keys)
-		$table_keys =  ", $table_keys";
+			$table_keys =  ", $table_keys";
 
 		$table_structure = "( $table_columns $table_keys )";
+
+		$search_array = array();
+		$replace_array = array();
+
+		$search_array[] = "`";
+		$replace_array[] = "";
+
+		$table_structure = str_replace($search_array,$replace_array,$table_structure);
+
 		$sql = "CREATE TABLE $table_name $table_structure $charset_collate;";
 
 		// Rather than executing an SQL query directly, we'll use the dbDelta function in wp-admin/includes/upgrade.php (we'll have to load this file, as it is not loaded by default)
